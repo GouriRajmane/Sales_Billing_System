80a1ecce42fe0ad14bb12e15b8175b5e2108eab9
Gouri Rajmane <rajmanegouri@gmail.com>
2026-08-25 15:47:26 +0530

    Added Model classes Product_Master and Customer_Mater

--- Sales_Billing_System/Models/Customer_Master.cs
+++ Sales_Billing_System/Models/Customer_Master.cs
@@ -1,10 +1,35 @@
 ﻿using Microsoft.Extensions.Options;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 namespace Sales_Billing_System.Models
 {
     public class Customer_Master
     {
+        // - Customer ID
+        //- Customer Name
+        //- Mobile Number
+        //- Address
+        //- GSTIN(optional)
 
+        [Key]
+        public int CustomerId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string CustomerName { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string MobileNumber { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Address { get; set; }
+
+
+        [StringLength(15)]
+        public string? GSTIN { get; set; }
 
 
     }
